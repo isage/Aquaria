9155759f878e4c2e0c1169afbb0d6d2bf4e6832f
Ivan Epifanov <isage.dna@gmail.com>
2026-08-24 23:42:05 +0300

    Simplify and cleanup

--- BBGE/Flags.h
+++ BBGE/Flags.h
@@ -21,15 +21,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef BBGE_FLAGS_H
 #define BBGE_FLAGS_H
 
-#ifdef BBGE_BUILD_WINDOWS
-	typedef unsigned __int32	uint32;
-#endif
-#ifdef BBGE_BUILD_UNIX
-	#include <stdint.h>
-	typedef uint32_t			uint32;
-#endif
 #ifdef BBGE_BUILD_X360
 	typedef unsigned int		uint32;
+#else
+	#include <stdint.h>
+	typedef uint32_t			uint32;
 #endif
 
 class Flags
